7cebea52166b6f3fecb9552db37dbb39ab8638ba
Ivan Yelizariev <yelizariev@it-projects.info>
2018-04-24 01:57:29 +0500

    Put data and demo attributes together

--- docs/dev/docs/templates/__manifest__.py
+++ docs/dev/docs/templates/__manifest__.py
@@ -26,11 +26,11 @@
         "{FILE1}.xml",
         "{FILE2}.xml",
     ],
-    "qweb": [
-        "static/src/xml/{QWEBFILE1}.xml",
-    ],
     "demo": [
         "demo/{DEMOFILE1}.xml",
+    ],
+    "qweb": [
+        "static/src/xml/{QWEBFILE1}.xml",
     ],
 
     "post_load": None,
